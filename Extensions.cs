33ce28fff1aa5d999ecee9b9d858f2dd2661638d
frederik-hoeft <43813142+Th3-Fr3d@users.noreply.github.com>
2020-08-28 17:54:03 +0200

    Prepared Source Code For Public Release

--- Extensions.cs
+++ Extensions.cs
@@ -1,25 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace fftc
 {
+    /// <summary>
+    /// Provides any extensions needed by fftc.
+    /// </summary>
     public static class Extensions
     {
-        public static string ToHumanReadableFileSize(this double fileSize, int decimals)
+        /// <summary>
+        /// Converts a large number of bytes to a more readable formatted string including the units.
+        /// </summary>
+        /// <param name="fileSize">The number of bytes to be converted.</param>
+        /// <param name="decimals">The amount of decimals to round the result to (if any).</param>
+        public static string ToHumanReadableFileSize(this ulong fileSize, int decimals)
         {
             string[] units = new string[] { "B", "KB", "MB", "GB", "TB", "PB" };
             int i = 0;
-            while (fileSize > 1000)
+            double fileSizeDouble = fileSize;
+            while (fileSizeDouble > 1000)
             {
-                fileSize /= 1000;
-                fileSize = Math.Round(fileSize, decimals, MidpointRounding.AwayFromZero);
+                fileSizeDouble /= 1000;
+                fileSizeDouble = Math.Round(fileSizeDouble, decimals, MidpointRounding.AwayFromZero);
                 if (i < units.Length)
                 {
                     i++;
                 }
             }
-            return fileSize.ToString() + units[i];
+            return fileSizeDouble.ToString() + units[i];
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,10 +5,21 @@ using System.Net.Sockets;
 
 namespace fftc
 {
+    /// <summary>
+    /// Represents the main class of fftc.
+    /// </summary>
     internal static class Program
     {
+        /// <summary>
+        /// The current version of fftc in the following format: <Major>.<Month>-<Minor (Reset every month)>.Year
+        /// </summary>
         private const string VERSION = "v1.8-1.20";
-        static void Main(string[] args)
+
+        /// <summary>
+        /// The main entry point of the application.
+        /// </summary>
+        /// <param name="args">The command line parameters.</param>
+        private static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Fast File Transfer Client " + VERSION);
             if (args.Length >= 1)
@@ -18,10 +29,12 @@ namespace fftc
                     case "-l":
                         Receive(args);
                         break;
+
                     case "-c":
                     case "-s":
                         Send(args);
                         break;
+
                     default:
                         Usage();
                         break;
@@ -32,6 +45,20 @@ namespace fftc
                 Usage();
             }
         }
+
+        /// <summary>
+        /// Sends the specified file to the specified destination.
+        /// </summary>
+        /// <param name="args">
+        /// The array of command line arguments where:
+        ///     <list type="bullet">
+        ///         <item><c><paramref name="args"/>[0]</c> doesn't matter and can be <c>null</c>.</item>
+        ///         <item><c><paramref name="args"/>[1]</c> is the IPv4 address of the destination.</item>
+        ///         <item><c><paramref name="args"/>[2]</c> is the destination port.</item>
+        ///         <item><c><paramref name="args"/>[3]</c> currently must be <c>-f</c> to specify that a file is being transmitted (may add support for directory structures in the future).</item>
+        ///         <item><c><paramref name="args"/>[4]</c> is path of the file to be transmitted.</item>
+        ///     </list>
+        /// </param>
         private static void Send(string[] args)
         {
             if (args.Length != 5)
@@ -84,13 +111,27 @@ namespace fftc
                 Console.WriteLine("Error: could not connect to peer.");
                 return;
             }
-            Console.WriteLine("Sending data...");
-            socket.SendFile(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + file);
+            string filePath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + file;
+            Console.WriteLine("Sending " + ((ulong)new FileInfo(filePath).Length).ToHumanReadableFileSize(1) + " of data. This may take a while ...");
+            socket.SendFile(filePath);
             socket.Shutdown(SocketShutdown.Both);
             socket.Disconnect(true);
             socket.Close();
             Console.WriteLine("Done!");
         }
+
+        /// <summary>
+        /// Listens for a data at the specified port and writes it to the specified file.
+        /// </summary>
+        /// <param name="args">
+        /// The array of command line arguments where:
+        ///     <list type="bullet">
+        ///         <item><c><paramref name="args"/>[0]</c> doesn't matter and can be <c>null</c>.</item>
+        ///         <item><c><paramref name="args"/>[1]</c> is the local port to listen on.</item>
+        ///         <item><c><paramref name="args"/>[2]</c> currently must be <c>-f</c> to specify that a file is being received (may add support for directory structures in the future).</item>
+        ///         <item><c><paramref name="args"/>[3]</c> is the file the data will be written to.</item>
+        ///     </list>
+        /// </param>
         private static void Receive(string[] args)
         {
             if (args.Length != 4)
@@ -98,7 +139,7 @@ namespace fftc
                 Usage();
                 return;
             }
-            if (args[2] != "-f")
+            if (!args[2].Equals("-f"))
             {
                 Usage();
                 return;
@@ -132,11 +173,11 @@ namespace fftc
                 if (counter >= 16)
                 {
                     counter = 0;
-                    Console.Write("\rReceived: " + ((double)rec).ToHumanReadableFileSize(1) + " ...");
+                    Console.Write("\rReceived: " + rec.ToHumanReadableFileSize(1) + " ...");
                     fileStream.Flush();
                 }
             }
-            Console.Write("\rReceived: " + ((double)rec).ToHumanReadableFileSize(1) + " ...\n");
+            Console.Write("\rReceived: " + rec.ToHumanReadableFileSize(1) + " ...\n");
             fileStream.Flush();
             fileStream.Close();
             try
@@ -154,6 +195,10 @@ namespace fftc
             Console.WriteLine(rec.ToString() + " bytes written to " + file);
             Console.WriteLine("Closed connection to " + remoteEndPoint.Address);
         }
+
+        /// <summary>
+        /// Displays usage information
+        /// </summary>
         private static void Usage()
         {
             Console.WriteLine("Fast File Transfer Client " + VERSION);
@@ -163,13 +208,20 @@ namespace fftc
                 "Send a file to a specified ip and port:\n" +
                 "           fftc [-c|-s] <ip> <port> -f <filename>\n" +
                 "Listen for a file at a specified port:\n" +
-                "           fftc -l <port> -f <filename>\n\n" +
-                "Examples:\n\n" +
+                "           fftc -l <port> -f <filename>\n" +
+                "\n" +
+                "Examples:\n" +
+                "\n" +
                 "Send a file called \"myfile.zip\" to 10.1.1.12, port 12344:\n" +
                 "fftc -s 10.1.1.12 12344 -f myfile.zip\n" +
+                "\n" +
                 "Listen for a file called \"myfile.zip\" at port 12344:\n" +
                 "fftc -l 12344 -f myfile.zip\n");
         }
+
+        /// <summary>
+        /// Gets the <see cref="IPAddress"/> of the local network interface to listen on.
+        /// </summary>
         private static IPAddress GetLocalIPAddress()
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
